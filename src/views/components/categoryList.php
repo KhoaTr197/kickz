be83f2f6d93e940a0d2fad814dc9c6697a37609f
Khoa Tr <darkreaper197@gmail.com>
2024-09-30 22:24:03 +0700

    Code xong header, Thiếu phần logic

---
+++ src/views/components/categoryList.php
@@ -0,0 +1,8 @@
+<ul class="category-list">
+  <li class="category-header">Nam</li>
+  <li class="category-item">Nike</li>
+  <li class="category-item">Adidas</li>
+  <li class="category-item">Puma</li>
+  <li class="category-item">Converse</li>
+  <li class="category-item">Ananas</li>
+</ul>
