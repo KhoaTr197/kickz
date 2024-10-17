606871440182df09c473ecdfa6b425353b372b24
Khoa Tr <darkreaper197@gmail.com>
2024-10-17 18:29:24 +0700

    Chỉnh sửa lại file Homepage.php

--- src/views/homepage.php
+++ src/views/homepage.php
@@ -1,3 +1,13 @@
+<?php
+  include("components/components.php");
+
+  $header_html = header_render("navbar");
+  $landingPage_html = landingPage_render("navbar");
+  $filterPanel = filterPanel_render();
+  $productList = productList_render();
+  $footer_html = footer_render();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,35 +16,30 @@
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="public/css/base.css">
-  <link rel="stylesheet" href="public/css/grid.css">
-  <link rel="stylesheet" href="public/css/main.css">
-  <script src="public/js/jquery-3.7.1.min.js"></script>
+  <link rel="stylesheet" href="../../public/css/base.css">
+  <link rel="stylesheet" href="../../public/css/grid.css">
+  <link rel="stylesheet" href="../../public/css/main.css">
+  <script src="../../public/js/jquery-3.7.1.min.js"></script>
   <title>Kickz</title>
 </head>
 <body>
   <div id="app" class="grid">
     <?php 
-      include 'components/header.php';
-      include 'components/landingPage.php';
-      include 'components/main.php';
-      include 'components/footer.php';
-      
-      function homepage_render() {
-        $Header = header_render("header");
-        $LandingPage = landingPage_render();
-        $Main = main_render();
-        $Footer = footer_render();
-
-        echo "
-          $Header
-          $LandingPage
-          $Main
-          $Footer
-        ";
-      }
+      echo "
+        $header_html
+        $landingPage_html
+        <main class='main'>
+          <div class='wide'>
+            <div class='row'>
+              $filterPanel
+              $productList
+            </div>
+          </div>
+        </main>
+        $footer_html
+      ";
     ?>
   </div>
-  <script src="public/js/app.js"></script>
+  <script src="../../public/js/app.js"></script>
 </body>
 </html>
