4dcae39b4f1f9b6cf9cfb19fa42cb7b16c0ca45a
Khoa Tr <darkreaper197@gmail.com>
2024-10-17 18:38:10 +0700

    Tạo mới file components.php, phụ trách việc import tất cả các components.
    Khi nào sử dụng thì chỉ cần include file này mà gọi đến hàm đó tránh việc include nhiều lần làm rối code

---
+++ src/views/components/components.php
@@ -0,0 +1,9 @@
+<?php
+  include_once("components/header.php");
+  include_once("components/loginForm.php");
+  include_once("components/landingPage.php");
+  include_once("components/filterPanel.php");
+  include_once("components/productCard.php");
+  include_once("components/productList.php");
+  include_once("components/footer.php");
+?>
