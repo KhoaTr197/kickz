86ab1395bf351435dc8649a41d95d4262942d7e1
Khoa Tr <darkreaper197@gmail.com>
2024-10-22 11:23:16 +0700

    tạo 2 trang đăng nhập-đăng ký

--- src/views/login.php
+++ src/views/login.php
@@ -1,8 +1,13 @@
 <?php
   include("components/components.php");
+  session_start();
 
   $header_html = header_render("login");
-  $loginForm_html = loginForm_render();
+  $error='';
+
+  if(!empty($_SESSION) && !empty($_SESSION['LOGIN_ERROR_PROMPT'])) {
+    $error="<div class='form-error flex rounded'>".$_SESSION['LOGIN_ERROR_PROMPT']."</div>";
+  }
 ?>
 
 <!DOCTYPE html>
@@ -17,6 +22,7 @@
   <link rel="stylesheet" href="../../public/css/grid.css">
   <link rel="stylesheet" href="../../public/css/main.css">
   <script src="../../public/js/jquery-3.7.1.min.js"></script>
+  <script src="../../public/js/app.js"></script>
   <title>Kickz</title>
 </head>
 <body>
@@ -26,11 +32,36 @@
       <main class='main'>
         <div class='wide'>
           <div class='row'>
-            $loginForm_html
+            <div class='c-12 flex-center'>
+                <form class='form' id='login-form' action='../controllers/loginController.php' method='post'>
+                  $error
+                  <div class='form-control-wrap'>
+                    <h2 class='form-title font-medium'>Đăng Nhập</h2>
+                    <div class='input-group'>
+                      <input class='form-input' type='text' placeholder='Tên Tài Khoản' name='username'/>
+                      <input class='form-input' type='password' placeholder='Mật Khẩu' name='password'/>
+                    </div>
+                    <label class='checkbox'>
+                      <div class='checkbox-wrap'>
+                        <input type='checkbox'>
+                        <span class='checkmark'></span>
+                      </div>
+                      <span class='checkbox-label'>Nhớ lần đăng nhập này</span>
+                    </label>
+                  </div>
+                  <button class='form-submit-btn btn btn-primary' type='submit'>Đăng Nhập</button>
+                  <div class='other-cta-container'>
+                    <span>Chưa có tài khoản?</span>
+                    <a class='other-cta__link' href='signup.php'>Đăng Ký</a>
+                  </div>
+                </form>       
+              </div>
+            </div> 
           </div>
         </div>
       </main>
     ";
+    unset($_SESSION['LOGIN_ERROR_PROMPT']);
   ?>
 </body>
 </html>
---
+++ src/views/signup.php
@@ -0,0 +1,62 @@
+<?php
+  include("components/components.php");
+  session_start();
+
+  $header_html = header_render("login");
+  $error='';
+
+  if(!empty($_SESSION) && !empty($_SESSION['SIGNUP_ERROR_PROMPT'])) {
+    $error="<div class='form-error flex rounded'>".$_SESSION['SIGNUP_ERROR_PROMPT']."</div>";
+  }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="../../public/css/base.css">
+  <link rel="stylesheet" href="../../public/css/grid.css">
+  <link rel="stylesheet" href="../../public/css/main.css">
+  <script src="../../public/js/jquery-3.7.1.min.js"></script>
+  <title>Kickz</title>
+</head>
+<body>
+  <?php
+    echo "
+      $header_html
+      <main class='main'>
+        <div class='wide'>
+          <div class='row'>
+            <div class='c-12 flex-center'>
+
+              <form class='form' action='../controllers/signupController.php' method='post'>
+                $error
+                <div class='form-control-wrap'>
+                  <h2 class='form-title font-medium'>Đăng Ký</h2>
+                  <div class='input-group'>
+                    <input class='form-input' type='text' placeholder='Tên Tài Khoản' name='username'/>
+                    <input class='form-input' type='password' placeholder='Mật Khẩu' name='password'/>
+                    <input class='form-input' type='password' placeholder='Xác Nhận Mật Khẩu' name='confirm_password'/>
+                    <div class='form-reminder'>Mật khẩu dài ít nhất 8 ký tự, chứa số, chữ cái in hoa, không chứa khoảng trắng, ký tự đặc biệt</div>
+                  </div>
+                  </div>                  
+                <button class='form-submit-btn btn btn-primary' type='submit'>Đăng Ký</button>
+                <div class='other-cta-container'>
+                  <span>Đã có tài khoản?</span>
+                  <a class='other-cta__link' href='login.php'>Đăng Nhập</a>
+                </div>
+              </form>
+
+            </div> 
+          </div>
+        </div>
+      </main>
+    ";
+    unset($_SESSION['SIGNUP_ERROR_PROMPT']);
+  ?>
+</body>
+</html>
