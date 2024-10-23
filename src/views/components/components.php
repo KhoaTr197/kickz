bdc7ccb2b86238d502839d2edb179d49440a7616
Khoa Tr <darkreaper197@gmail.com>
2024-10-23 18:04:00 +0700

    Làm lại trang detail (CTSP)

--- src/views/components/components.php
+++ src/views/components/components.php
@@ -1,6 +1,5 @@
 <?php
   include_once("components/header.php");
-  include_once("components/landingPage.php");
   include_once("components/filterPanel.php");
   include_once("components/productCard.php");
   include_once("components/productList.php");
