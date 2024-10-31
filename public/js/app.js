3a77181d914b1b1ed177a73b62364a41a36f7f61
Khoa Tr <darkreaper197@gmail.com>
2024-10-31 21:55:55 +0700

    thêm code xử lý event click

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,23 @@
-$(document).ready(function() {
-  const menuModal = $("#menu-modal");
-  
+$(document).ready(function() {  
   $('#menu').click(function() {
-    console.log(1);
-    menuModal.toggle();
+    $("#menu-modal").toggle();
+  })
+
+  $('.sidebar__item').click(function() {
+    const sidebarItem = this.id.split('_');
+
+    $('.sidebar__item').removeClass('active');
+    $(this).addClass('active');
+
+    $(`.user-panel__item`).removeClass('active');
+    $(`#${sidebarItem[0]}_modal`).addClass('active');
+  })
+
+  $('.quantity-btn__minus').click(function() {
+    this.parentNode.querySelector(".quantity-btn__input").stepDown();
+  })
+
+  $('.quantity-btn__add').click(function() {
+    this.parentNode.querySelector(".quantity-btn__input").stepUp();
   })
 })
