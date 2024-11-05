673296075e57bab9ff104db1f85b35b88481eb2b
Khoa Tr <darkreaper197@gmail.com>
2024-11-05 21:41:12 +0700

    sửa lại cart, viết thêm phần logic để hiển thị dữ liệu thực tế

--- src/views/cart.php
+++ src/views/cart.php
@@ -1,12 +1,21 @@
 <?php
-  include_once("components/components.php");
+require_once("../models/Database.php");
+include_once("components/components.php");
 
-  $header_html = header_render("breadcrumb");
-  $footer_html = footer_render();
+$header_html = header_render("breadcrumb");
+$footer_html = footer_render();
+
+$db = new Database();
+$sql = "
+    select *
+    from product
+  ";
+$result = $db->query($sql);
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -20,43 +29,47 @@
   <script src="../../public/js/app.js"></script>
   <title>Kickz</title>
 </head>
-<body>
+
+<body onunload="return myFunction()">
   <div id="app" class="grid">
-    <?php echo $header_html?>
-      <main class='main'>
-        <div class='wide'>
-          <div class='row'>
-            <div class="cart-list col c-8">
+    <?php echo $header_html ?>
+    <main class='main'>
+      <div class='wide'>
+        <form action='../controllers/checkout.php' method='post' id="cart-list" class='row'>
+          <div class="col c-8">
+            <ul class="cart-list">
               <h2 class='cart-list__title'>Giỏ Hàng</h2>
-              <div class='cart-list__item flex rounded'>
-                <img src="https://images.stockx.com/images/Air-Jordan-3-Retro-OG-SP-A-Ma-Maniere-Black-Violet-Ore-Womens-Product.jpg?fit=fill&bg=FFFFFF&w=120&h=100&auto=compress&dpr=1&trim=color&updated_at=1724151308&fm=webp&q=60">
-                <div class="cart-info-1 flex">
-                  <div class="cart-info-1__name font-medium">
-                    Jordan 3 Retro OG SP A Ma Maniére While You Were Sleeping (Women's)
-                  </div>
-                  <div class="cart-info-1__quantity-btn flex-center rounded">
-                    <button class="quantity-btn__minus flex-center">
-                      <img src='../../public/img/minus_icon.svg'/>
-                    </button>
-                    <input class="quantity-btn__input font-medium" type="number" min='0' value='1' />
-                    <button class="quantity-btn__add flex-center">
-                      <img src='../../public/img/plus_icon.svg'/>                     
-                    </button>
-                  </div>
-                </div>
-                  <div class="cart-info-2 flex">
-                    <a class="cart-info-2__delete-btn" href='#'>
-                      <img src="../../public/img/trashcan_icon.svg">
-                    </a>
-                    <div class="cart-info-2__price font-medium">3.699.999đ</div>
-                </div>
-              </div>            
-            </div>
-            <div class="col c-4">456</div>
+              <?php
+              while ($row = $db->fetch($result)) {
+                echo productCard_render($row, 'cart');
+              }
+              ?>
+            </ul>
           </div>
-        </div>
-      </main>
+          <div class="col c-4">
+            <div class="checkout flex">
+              <div class="checkout__price-list flex">
+                <div class="price-list__item">
+                  <div class="price-list-item__name">Tổng Tiền Sản Phẩm</div>
+                  <div class="price-list-item__price">16.000.000đ</div>
+                </div>
+                <div class="price-list__item">
+                  <div class="price-list-item__name">Phí Vận Chuyển</div>
+                  <div class="price-list-item__price">100.000đ</div>
+                </div>
+              </div>
+              <div class="checkout__total-price flex font-semibold">
+                <div class="checkout-total-price__name">Tổng Tiền</div>
+                <div class="checkout-total-price__price">16.100.000đ</div>
+              </div>
+              <button id='checkout-btn' type="submit" class="checkout__checkout-btn btn btn-primary font-semibold">Xác Nhận Đơn Hàng</button>
+            </div>
+          </div>
+        </form action='../controllers/checkout.php' method='post'>
+      </div>
+    </main>
     <?php echo $footer_html; ?>
   </div>
 </body>
+
 </html>
