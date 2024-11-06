80aaa52a2624639e4822428fe71e11252c6bc3b7
Hyu0582 <hkhuy2208@gmail.com>
2024-11-06 23:20:41 +0700

    thêm css cho page btn

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,28 @@
-const menuModal = $("#menu-modal")
+$(document).ready(function() {  
+  $('#menu').click(function() {
+    $("#menu-modal").toggle();
+  })
 
-$('#menu').click(function() {
-  menuModal.toggle()
+  $('.sidebar__item').click(function() {
+    const sidebarItem = this.id.split('_');
+
+    $('.sidebar__item').removeClass('active');
+    $(this).addClass('active');
+
+    $(`.user-panel__item`).removeClass('active');
+    $(`#${sidebarItem[0]}_modal`).addClass('active');
+  })
+
+  $('.quantity-btn__minus').click(function() {
+    let element = $(this).siblings('.quantity-btn__input')[0];
+    element.stepDown();
+    element.dispatchEvent(new Event('change'));
+  })
+
+  $('.quantity-btn__add').click(function() {
+    let element = $(this).siblings('.quantity-btn__input')[0];
+    element.stepUp();
+    element.dispatchEvent(new Event('change'));
+  })
 })
+
