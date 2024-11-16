4da30af5c0ce6468e62a42fef97a6e512e2e10c0
Khoa Tr <darkreaper197@gmail.com>
2024-11-16 21:59:52 +0700

    Chuyển hàm sang file khác

--- src/controllers/imageController.php
+++ src/controllers/imageController.php
@@ -8,49 +8,4 @@
     }
     return true;
   }
-  function insertImages($db, $arrayImg) {
-    global $db;
-
-    foreach($arrayImg['tmp_name'] as $i => $name) {
-      $filename = explode('-', substr($arrayImg['name'][$i], 0, -4));
-      $file = $arrayImg["tmp_name"][$i];
-      $image = file_get_contents($file);
-
-      $idx=[
-        'right' => 1,
-        'front' => 2,
-        'left' => 3,
-        'back' => 4
-      ];
-      $id = (int)$filename[1];
-      $imageSQL = "";
-      $stmt = null;
-
-      switch($filename[0]) {
-        case 'product':
-          $imageSQL = "
-            insert ignore into HINHANH (MASP, MAHA, URL)
-            values(?, ?, ?)
-          ";
-          $stmt = $db->prepare($imageSQL);
-          mysqli_stmt_bind_param($stmt, 'iis', $id, $idx[$filename[2]], $image);
-          $db->stmt_execute($stmt);
-          break;
-        case 'manufacturer':
-          $imageSQL = "
-            update HANGSANXUAT
-            set LOGO = ?
-            where MAHSX = ? 
-          ";
-          echo $imageSQL;
-          $stmt = $db->prepare($imageSQL);
-          mysqli_stmt_bind_param($stmt, 'si', $image, $id);
-          $db->stmt_execute($stmt);
-          break;
-      }
-
-      header("location: index.php");
-      
-    }
-  }
 ?>
