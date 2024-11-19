6a70dfe602131e7f6940a6b4561080b40528bcea
Khoa Tr <darkreaper197@gmail.com>
2024-11-19 21:14:09 +0700

    Sửa NGLAPSP -> NGSX

--- src/utils/utils.php
+++ src/utils/utils.php
@@ -1,44 +1,45 @@
 <?php
-  function formatPrice($price) {
-    $newPrice = number_format($price, 0, '', '.');
-    return "$newPrice"."đ";
-  }
-  function formatSQLColumnsName($col) {
-    $ref = [
-      //SANPHAM
-      'MASP' => 'Mã',
-      'TENSP' => 'Tên Sản Phẩm',
-      'GIA' => 'Giá',
-      'MOTA' => 'Mô Tả',
-      'SOSAO' => 'Đánh Giá', 
-      'TENHSX' => 'Tên Hãng',
-      'NGLAPSP' => 'Ngày Sản Xuất',
-      'KHUYENMAI' => 'Khuyến Mãi',
-      'TRANGTHAI' => 'Trạng Thái',
-      //HSX
-      'MAHSX' => 'Mã HSX',
-      'LOGO' => 'Logo',
-      //DANHMUC
-      'MADM' => 'Mã Danh Mục',
-      'TENDM' => 'Tên Danh Mục',
-      'MAKC' => 'Mã Kích Cỡ',
-      'COGIAY' => 'Cỡ Giày',
-      'SOLUONG' => 'Số Lượng',
-      //HOADON
-      'MAHD' => '',
-      'MATK' => '',
-      'HOTENKH' => '',
-      'EMAIL' => 'Email',
-      'SDT' => 'SĐT',
-      'DCHI' => 'Địa Chỉ',
-      'GHICHU' => 'Ghi Chú',
-      'NGLAPHD' => 'Ngày Lập Hóa Đơn',
-      'MATT' => 'Mã Trạng Thái',
-      //NGUOIDUNG
-      'TENTK' => 'Tên Tài Khoản',
-      'HOTEN' => 'Họ Tên',
-      'NGLAPTK' => 'Ngày Lập Tài Khoản',
-    ];
-    return $ref[$col];
-  }
-?>
+function formatPrice($price)
+{
+  $newPrice = number_format($price, 0, '', '.');
+  return "$newPrice" . "đ";
+}
+function formatSQLColumnsName($col)
+{
+  $ref = [
+    //SANPHAM
+    'MASP' => 'Mã',
+    'TENSP' => 'Tên Sản Phẩm',
+    'GIA' => 'Giá',
+    'MOTA' => 'Mô Tả',
+    'SOSAO' => 'Đánh Giá',
+    'TENHSX' => 'Tên Hãng',
+    'NGSX' => 'Ngày Sản Xuất',
+    'KHUYENMAI' => 'Khuyến Mãi',
+    'TRANGTHAI' => 'Trạng Thái',
+    //HSX
+    'MAHSX' => 'Mã HSX',
+    'LOGO' => 'Logo',
+    //DANHMUC
+    'MADM' => 'Mã Danh Mục',
+    'TENDM' => 'Tên Danh Mục',
+    'MAKC' => 'Mã Kích Cỡ',
+    'COGIAY' => 'Cỡ Giày',
+    'SOLUONG' => 'Số Lượng',
+    //HOADON
+    'MAHD' => '',
+    'MATK' => '',
+    'HOTENKH' => '',
+    'EMAIL' => 'Email',
+    'SDT' => 'SĐT',
+    'DCHI' => 'Địa Chỉ',
+    'GHICHU' => 'Ghi Chú',
+    'NGLAPHD' => 'Ngày Lập Hóa Đơn',
+    'MATT' => 'Mã Trạng Thái',
+    //NGUOIDUNG
+    'TENTK' => 'Tên Tài Khoản',
+    'HOTEN' => 'Họ Tên',
+    'NGLAPTK' => 'Ngày Lập Tài Khoản',
+  ];
+  return $ref[$col];
+}
