17717e188bde2a112ebfb17a220bfb4c3589f5dc
Khoa Tr <darkreaper197@gmail.com>
2024-11-23 22:37:07 +0700

    bổ sung js

--- public/js/app.js
+++ public/js/app.js
@@ -3,6 +3,16 @@ $(document).ready(function() {
     $("#menu-modal").toggle();
   })
 
+  $('.sidebar__item').click(function() {
+    const sidebarItem = this.id.split('_');
+
+    $('.sidebar__item').removeClass('active');
+    $(this).addClass('active');
+
+    $(`.user-panel__item`).removeClass('active');
+    $(`#${sidebarItem[0]}_modal`).addClass('active');
+  })
+
   $('.quantity-btn__minus').click(function() {
     let element = $(this).siblings('.quantity-btn__input')[0];
     element.stepDown();
