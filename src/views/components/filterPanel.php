9c2ae0ce8a539236f25cc7e8e394dbb77051569a
KhoaTr197 <darkreaper197@gmail.com>
2024-12-03 11:41:30 +0700

    -Làm lại filter panel:
    -Ý tưởng là truyền dữ liệu vào trong và trả về html

--- src/views/components/filterPanel.php
+++ src/views/components/filterPanel.php
@@ -1,56 +1,79 @@
 <?php
-function filterPanel_render()
+function filterPanel_render(
+  $filterListData=[],
+  $mode='button',
+  $layout='row'
+)
 {
-  
-  $priceList_html = filterList_render("price");
-  $manufacturerList_html = filterList_render("manufacturer");
-
-  return "
-      <div id='filter-panel' class='col l-2 c-12 no-gutter'>
-        <div class='filter-panel-wrap'>
-          $priceList_html     
-        </div>
-      </div>
-    ";
-}
-function filterList_render($mode) {
   $newQueryStr = "";
-  $priceList = [
-    'less-1000000' => 'Dưới 1.000.000đ',
-    '1000000-5000000' => 'Từ 1.000.000đ - 5.000.000đ',
-    '5000000-10000000' => 'Từ 5.000.000đ - 10.000.000đ',
-    'more-10000000' => 'Trên 10.000.000đ'
-  ];
-  $html = "";
+  $filterList_html = "";
+  $gridClass = $layout == 'column' ? 'col l-2 c-12 no-gutter' : '';
+  
+  foreach($_GET as $key => $value) {
+    if(array_key_exists($key, $filterListData)) continue;
+    $newQueryStr .= "$key=$value&";
+  }
 
   switch($mode) {
-    case 'price':
-      foreach($_GET as $key => $value) {
-        if($key == 'price') continue;
-        $newQueryStr .= "$key=$value&";
+    case 'button': {
+      $filterItems="";
+      
+      foreach($filterListData as $filterListName => $filterList) {
+        if(!isset($_GET[$filterListName]))
+          return header("location: ?{$newQueryStr}$filterListName=none");
+        foreach($filterList as $filterKey => $filterValue) {
+          $isActived = (isset($_GET[$filterListName]) and $filterKey == $_GET[$filterListName]) ? 'filter-list__item--active' : '';
+
+          $filterItems .= "
+            <a class='filter-list__item $isActived flex flex-center btn rounded-lg' href='?{$newQueryStr}$filterListName=$filterKey'>$filterValue</a>
+          ";
+        }
       }
-      $html .= "
-      <ul class='filter-list' id='price'>
-        <h3 class='filter-list__title font-medium'>Giá Tiền</h3>
+      $filterList_html .= "
+        <ul class='filter-list flex'>
+          $filterItems
+        </ul>
       ";
-      foreach ($priceList as $price => $title) {
-        $isChecked = (isset($_GET['price']) and $price === $_GET['price']) ? 'checked' : '';
-        $html .= "
+      break;
+    }
+    case 'checkbox': {
+      $filterItems="";
+      
+      foreach($filterListData as $filterListName => $filterList) {
+        $filterItems .= "<li class='filter-list__title'>".formatName($filterListName)."</li>";
+        $filterItems .= '<ul>';
+        foreach($filterList as $filterKey => $filterValue) {
+          $isChecked = (isset($_GET[$filterListName]) and $filterKey == $_GET[$filterListName]) ? 'checked' : '';
+
+          $filterItems .= "
             <li class='filter-list__item'>
               <label class='checkbox'>
-                <div class='checkbox-wrap'>
-                  <input type='checkbox' value='price=$price' $isChecked>
-                  <span class='checkmark'></span>
-                </div>
-                <span class='checkbox-label'>$title</span>
+              <div class='checkbox-wrap'>
+                <input type='checkbox' value='$filterListName=$filterKey' $isChecked>
+                <span class='checkmark'></span>
+              </div>
+              <span class='checkbox-label'>$filterValue</span>
               </label>
             </li>
           ";
+        }
+        $filterItems .= '</ul>';
       }
-      $html .= "</ul>";
+      $filterList_html .= "
+        <ul class='filter-list'>
+          $filterItems
+        </ul>
+      ";
       break;
+    }
   }
 
-  return $html;
+  return "
+    <div id='filter-panel' class='filter-panel--$layout $gridClass'>
+      <div class='filter-panel-wrap'>
+        $filterList_html
+      </div>
+    </div>
+  ";
 }
 ?>
